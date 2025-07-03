a7d36104af3e8dd241ab26a3580fde5a879826ab
houch <houchuang@outlook.com>
2025-07-03 21:46:44 +0800

    add gtest gmock demo

---
+++ pe.sh
@@ -0,0 +1,11 @@
+sudo apt update
+sudo apt install build-essentials -y
+sudo apt install cmake -y
+sudo apt install clang -y
+sudo apt install libpcap-dev -y
+sudo apt install libboost-all-dev -y
+sudo apt install gdb -y
+sudo apt install zip -y
+sudo apt install unzip -y
+sudo apt install openjdk-8-jdk -y
+echo "export PS1='\u@\h:\W\$ '" >> .bashrc
--- test/TestDemo.cpp
+++ test/TestDemo.cpp
@@ -4,6 +4,31 @@
 
 #include <gtest/gtest.h>
 
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+// 定义一个接口
+class MyInterface {
+public:
+    virtual ~MyInterface() = default;
+    virtual int Foo(int x) = 0;
+};
+
+
+// 使用gmock生成Mock类
+class MockMyInterface : public MyInterface {
+public:
+    MOCK_METHOD(int, Foo, (int x), (override));
+};
+
+TEST(MockTestSuite, MockTestCase) {
+    MockMyInterface mock;
+    EXPECT_CALL(mock, Foo(5)).Times(1).WillOnce(testing::Return(10));
+
+    ASSERT_EQ(mock.Foo(5), 10);
+}
+
 // Demonstrate some basic assertions.
 TEST(HelloTest, BasicAssertions) {
     // Expect two strings not to be equal.
