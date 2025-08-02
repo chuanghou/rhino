9fa0279d293194556e52634457e2b96a7e0e3f60
houch <houchuang@outlook.com>
2025-08-02 09:08:43 +0800

    add rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,4 @@
 rm -rf build
 cmake -S . -B build
-cmake --build build
+cmake --build build -- -j4
 cmake --install build
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,30 @@
 #include <iostream>
 #include <string>
 
+
+
+
 #ifndef GTESTS
-int main() {
-
-
-    std::cout << "Hello World!" << std::endl;
-    return 0;
+#include <boost/program_options.hpp>
+int main(int argc, char* argv[]) {
+    boost::program_options::options_description opts("demo options");
+    opts.add_options()
+        ("about,a", "this is a about.")
+        ("version,v", "program version");//两个程序选项
+    boost::program_options::variables_map vm;//选项存储map容器
+    boost::program_options::store(boost::program_options::parse_command_line(argc, argv, opts), vm);//解析存储
+    if (vm.empty())
+    {
+        std::cout << "no options" << std::endl;
+        return;
+    }
+    if (vm.count("about"))
+    {
+        std::cout << std::endl << opts << std::endl;
+    }
+    if (vm.count("version"))
+    {
+        std::cout <<"version is V3.3.0" << std::endl;
+    }
 }
 #endif
