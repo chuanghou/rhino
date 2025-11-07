b278f11a2dc9e33e6059d82c8fcf9832d542b493
houch <houchuang@outlook.com>
2025-11-07 21:02:41 +0800

    add SqlLite3

--- scripts/pe.sh
+++ scripts/pe.sh
@@ -9,6 +9,7 @@ sudo apt install zip -y
 sudo apt install unzip -y
 sudo apt install openjdk-8-jdk -y
 sudo apt install libabsl-dev -y
+sudo apt install libsqlite3-dev -y
 echo "export PS1='\u@\h:\W\$ '" >> .bashrc
 
 # 安装 capnproto https://capnproto.org/install.html
